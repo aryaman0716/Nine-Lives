b420408c5fcf4fec487fc17b0f3bee97f77ca1b3
AryamanMukherjee <aryamanm03@gmail.com>
2026-06-06 12:26:58 +0100

    Implemented scoring

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text highScoreText;
+    private PlayerController player;
+    private float score;
+    private int highScore;
+
+    public int CurrentScore => Mathf.FloorToInt(score);
+    private void Start()
+    {
+        player = FindFirstObjectByType<PlayerController>();
+        score = 0;
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    private void Update()
+    {
+        if (!player.GameStarted)
+            return;
+
+        score += Time.deltaTime * 10f;
+        scoreText.text = "Score: " + Mathf.FloorToInt(score);
+        highScoreText.text = "Best: " + highScore;
+    }
+    public void SaveHighScore()
+    {
+        if (CurrentScore > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+        }
+    }
+}
